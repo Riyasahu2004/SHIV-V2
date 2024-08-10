b406ae2daf2bb5db4e790e5daf016a4325853c4f
Vaibhav Gupta <128729218+vaibhav200420@users.noreply.github.com>
2024-08-10 17:00:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ________________________________________________________________________________#
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Rajputshivsingh65/SHIV-V2",
+    "https://github.com/Riyasahu2004/SHIV-V2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
