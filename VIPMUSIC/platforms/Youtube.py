f7ed9077128ed05cb5cb7145c1c7542885ceb11d
Riya Sahu <152384568+Riyasahu2004@users.noreply.github.com>
2024-08-10 19:38:23 +0530

    Update Youtube.py

--- VIPMUSIC/platforms/Youtube.py
+++ VIPMUSIC/platforms/Youtube.py
@@ -11,8 +11,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from VIPMUSIC.utils.database import is_on_off
-from VIPMUSIC.utils.formatters import time_to_seconds, download_file
+from DAXXMUSIC.utils.database import is_on_off
+from DAXXMUSIC.utils.formatters import time_to_seconds, download_file
 
 
 
